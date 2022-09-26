28375fb7b992e8396f3baf6e370eef8704bf3531
chaojin0310 <chaojin@pku.edu.cn>
2022-09-26 10:19:16 +0800

    update ceph_install

--- ceph_install.sh
+++ ceph_install.sh
@@ -3,25 +3,25 @@
 # admission controller
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.7.1/cert-manager.yaml
 
-# rook
+# install rook
 cd ~/data
 git clone --single-branch --branch v1.10.1 https://github.com/rook/rook.git
 cd rook/deploy/examples
 kubectl create -f crds.yaml -f common.yaml -f operator.yaml
 kubectl create -f cluster.yaml
 
-# toolbox
+# install ceph toolbox
 kubectl create -f toolbox.yaml
 kubectl -n rook-ceph rollout status deploy/rook-ceph-tools
 
-# object storage (s3-like)
+# intsall object storage (s3-like)
 kubectl create -f object.yaml
 kubectl create -f storageclass-bucket-delete.yaml
 kubectl create -f object-bucket-claim-delete.yaml
 
 cd ~
 
-# client connection
+# export client connection metadata as environment variables
 echo "export AWS_HOST=$(kubectl -n default get cm ceph-delete-bucket -o jsonpath='{.data.BUCKET_HOST}')" >> ~/.bashrc
 echo "export PORT=$(kubectl -n default get cm ceph-delete-bucket -o jsonpath='{.data.BUCKET_PORT}')" >> ~/.bashrc
 echo "export BUCKET_NAME=$(kubectl -n default get cm ceph-delete-bucket -o jsonpath='{.data.BUCKET_NAME}')" >> ~/.bashrc
